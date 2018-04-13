ffd63fde96f6ff14c05f00997e8100c8dc7ce1e0
anoimar <vladyslaw@yahoo.com>
2018-04-13 21:56:52 +0200

    changes

--- src/main/kotlin/com/thernat/superhero/Application.kt
+++ src/main/kotlin/com/thernat/superhero/Application.kt
@@ -2,20 +2,15 @@ package com.thernat.superhero
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+
 
 
 @SpringBootApplication
-@RestController
 class Application
 
-@RequestMapping("/")
-fun home(): String {
-    return "Hello Docker World"
-}
+
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
-
+    print("Started")
 }
